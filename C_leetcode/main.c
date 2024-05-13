cf32e8aeccf24f6f264ca66eb3d56fd6ab9a360b
alextan <alexmercertan@outlook.com>
2024-05-13 16:45:03 +0800

    Initial Commit

---
+++ C_leetcode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_leetcode
+//
+//  Created by alextan on 2024/5/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
